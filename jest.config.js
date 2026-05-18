d33164c37f26175b63c44fcf31bf3950bb2bbde9
Kev <95289692+KEVINLEE000@users.noreply.github.com>
2026-05-19 03:53:47 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/tests/setupJest.js'],
+  testPathIgnorePatterns: ['<rootDir>/main.test.js'],
   collectCoverageFrom: [
-    '**/*.js',
-    '!**/node_modules/**',
-    '!**/tests/**',
-    '!**/coverage/**',
-    '!jest.config.js',
+    'main.js',
+    'api/**/*.js',
+    'dateUtils.js',
+    'i18n.js',
   ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
